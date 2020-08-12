181c8609fe23a4feb6599596a5643e3fbd3a07aa
曾鹏 <2031481080@qq.com>
2020-08-12 15:00:35 +0800

    'fix'

--- mall-search/src/main/java/com/macro/mall/search/domain/EsProduct.java
+++ mall-search/src/main/java/com/macro/mall/search/domain/EsProduct.java
@@ -20,30 +20,111 @@ import java.util.List;
 @Document(indexName = "pms", type = "product",shards = 1,replicas = 0)
 public class EsProduct implements Serializable {
     private static final long serialVersionUID = -1L;
+
     @Id
     private Long id;
+
+    /**
+     * 货号
+     * keyword类型的字段只能通过精确值搜索到
+     */
     @Field(type = FieldType.Keyword)
     private String productSn;
+
+    /**
+     * 品牌id
+     */
     private Long brandId;
+
+    /**
+     * 品牌名称
+     */
     @Field(type = FieldType.Keyword)
     private String brandName;
+
+    /**
+     * 商品分类id
+     */
     private Long productCategoryId;
+
+    /**
+     * 商品分类名称
+     */
     @Field(type = FieldType.Keyword)
     private String productCategoryName;
+
+    /**
+     * 图片
+     */
     private String pic;
+
+    /**
+     * 商品名称
+     * @Field(analyzer = "ik_max_word",type = FieldType.Text):
+     *      当一个字段需要被全文搜索，可以设置为text类型，设置成text类型后，
+     *      该字段内容会被分析，在生成倒排索引之前，字符串会被分析器分成一个个词项
+     *
+     *      IK分词器有两种分词模式：ik_max_word和ik_smart模式。
+     *      ik_max_word:将文本做最细粒度的拆分
+     *      ik_smart_word:将文本做最粗粒度的拆分
+     *
+     *      索引时用ik_max_word,搜索时用ik_smart_word
+     */
     @Field(analyzer = "ik_max_word",type = FieldType.Text)
     private String name;
+
+    /**
+     * 副标题
+     */
     @Field(analyzer = "ik_max_word",type = FieldType.Text)
     private String subTitle;
+
+    /**
+     * 关键字
+     */
     @Field(analyzer = "ik_max_word",type = FieldType.Text)
     private String keywords;
+
+    /**
+     * 价格
+     */
     private BigDecimal price;
+
+    /**
+     * 销量
+     */
     private Integer sale;
+
+    /**
+     * 新品状态
+     */
     private Integer newStatus;
+
+    /**
+     * 推荐状态
+     */
     private Integer recommandStatus;
+
+    /**
+     * 库存
+     */
     private Integer stock;
+
+    /**
+     * 促销类型
+     */
     private Integer promotionType;
+
+    /**
+     * 排序
+     */
     private Integer sort;
+
+    /**
+     * 商品属性列表
+     * FieldType.Nested:嵌套类型，object中的一个特例，可以让array类型的
+     * Object独立索引和查询
+     */
     @Field(type =FieldType.Nested)
     private List<EsProductAttributeValue> attrValueList;
 }
--- mall-search/src/main/java/com/macro/mall/search/domain/EsProductAttributeValue.java
+++ mall-search/src/main/java/com/macro/mall/search/domain/EsProductAttributeValue.java
@@ -17,11 +17,14 @@ public class EsProductAttributeValue implements Serializable {
     private static final long serialVersionUID = 1L;
     private Long id;
     private Long productAttributeId;
+
     //属性值
     @Field(type = FieldType.Keyword)
     private String value;
+
     //属性参数：0->规格；1->参数
     private Integer type;
+
     //属性名称
     @Field(type=FieldType.Keyword)
     private String name;
